975fca0cf8bc21f1a53a6d07e9b9a6f172a961a5
unknown <mehedi15-2995@diu.edu.bd>
2022-08-09 18:08:55 +0600

    devisible added

---
+++ devisible.js
@@ -0,0 +1,25 @@
+//devisible.js
+/*
+    1.show output from : 1 to 50
+    2.if the number is devisible by 3 then instead of the number show "foo".
+    3.if the number is devisible by 5 then instead of the number show "bar".
+    4.if the number is devisible by both 3 and 5 then instead of the number show "foo-bar".
+
+*/
+for(let i = 1; i<=50;i++){
+    if(i % 3 ===0 && i%5 ===0){
+        console.log('foo-bar');
+    }
+    
+    else if(i%5===0){
+        console.log('bar');
+    }
+    else if(i%3===0){
+        console.log('foo')
+    }
+    else{
+        console.log(i)
+    }
+   
+    //console.log(i);
+}
